8b3063d3110b35617977fac84f00401c2ac55221
wasmenezes <was288519@gmail.com>
2020-12-18 06:54:53 -0300

    ensure ForecastController returns 500 if something goes wrong during the processing

--- src/controller/forecast.ts
+++ src/controller/forecast.ts
@@ -8,12 +8,14 @@ const forecast = new Forecast();
 @Controller('forecast')
 export class ForecastController {
   @Get('')
-  public async getForecastForLoggedUser(
-    _: Request,
-    res: Response
-  ): Promise<void> {
-    const beaches = await Beach.find({});
-    const forecastData = await forecast.processForecastForBeaches(beaches);
-    res.status(200).send(forecastData);
+  public async getForecastForLoggedUser(_: Request, res: Response): Promise<void> {
+    try {
+      const beaches = await Beach.find({});
+      const forecastData = await forecast.processForecastForBeaches(beaches);
+      res.status(200).send(forecastData);
+    } catch(error){
+      res.status(500).send({error: 'Something went wrong'})
+    }
+    
   }
 }
