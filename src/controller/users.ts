d8cda33e269efb2a80d0ac5999b24e811a763e03
wasmenezes <was288519@gmail.com>
2020-12-19 15:55:02 -0300

    ensure UserControler should return succesfully create a new user

---
+++ src/controller/users.ts
@@ -0,0 +1,13 @@
+import { Controller, Get, Post } from '@overnightjs/core';
+import { User } from '@src/models/user';
+import { Response, Request } from 'express';
+
+@Controller('users')
+export class UsersController {
+  @Post('')
+  public async create(req: Request, res: Response): Promise<void> {
+    const user = new User(req.body);
+    const result = await user.save();
+    res.status(201).send(result);
+  }
+}
