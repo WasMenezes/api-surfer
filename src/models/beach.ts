dac744a9ee4eaf944eeb7048636791041ce68e7b
wasmenezes <was288519@gmail.com>
2020-12-20 17:15:59 -0300

    ensure verification of JWT token and call the next middleware

--- src/models/beach.ts
+++ src/models/beach.ts
@@ -1,4 +1,4 @@
-import mongoose, { Document, Model } from 'mongoose';
+import mongoose, { Document, Model, Schema } from 'mongoose';
 
 export enum BeachPosition {
   S = 'S',
@@ -21,6 +21,7 @@ const schema = new mongoose.Schema(
     lng: { type: Number, required: true },
     name: { type: String, required: true },
     position: { type: String, required: true },
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   },
   {
     toJSON: {
@@ -33,5 +34,5 @@ const schema = new mongoose.Schema(
   }
 );
 
-interface BeachModel extends Omit<Beach, '_id'>, Document {}
+interface BeachModel extends Omit<Beach, '_id'>, Document { }
 export const Beach: Model<BeachModel> = mongoose.model('Beach', schema);
