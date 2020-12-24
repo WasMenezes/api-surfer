9090614060790dfa1d13741300ac94e46133cb21
wasmenezes <was288519@gmail.com>
2020-12-24 06:44:43 -0300

    ensure Rating alghoritm returns rating based on winds/wave

---
+++ src/services/rating.test.ts
@@ -0,0 +1,41 @@
+import { Beach, BeachPosition } from "@src/models/beach";
+import { Rating } from "./rating";
+
+describe('Rating Service', () => {
+  const defaultBeach: Beach = {
+    lat: -33.792726,
+    lng: 151.289824,
+    name: 'Manly',
+    position: BeachPosition.E,
+    user: 'some-user',
+  };
+
+  const defaultRating = new Rating(defaultBeach);
+
+  describe('Calculate rating for a given point', () => {
+    //TODO
+  });
+
+  describe('Get rating based on wind and wave positions', () => {
+    it('should get rating 1 for a beach with onshore winds', () => {
+      const rating = defaultRating.getRatingBasedOnWindAndWavePositions(BeachPosition.E, BeachPosition.E);
+      expect(rating).toBe(1);
+    });
+
+    it('shuold get rating 3 for a beach with cross winds', () => {
+      const rating = defaultRating.getRatingBasedOnWindAndWavePositions(
+        BeachPosition.E,
+        BeachPosition.S
+      );
+      expect(rating).toBe(3);
+    });
+
+    it('shuold get rating 5 for a beach with offshore winds', () => {
+      const rating = defaultRating.getRatingBasedOnWindAndWavePositions(
+        BeachPosition.E,
+        BeachPosition.W
+      );
+      expect(rating).toBe(5);
+    });
+  });
+});
