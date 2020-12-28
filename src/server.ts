a03f25d5727331a6802fcae8bdb74e580629f2d7
wasmenezes <was288519@gmail.com>
2020-12-28 05:29:56 -0300

    add and config OpenAPI

--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,17 @@ import { Application } from 'express';
 import * as http from 'http';
 import expressPino from 'express-pino-logger';
 import cors from 'cors';
+import apiSchema from './api.schema.json';
+import swaggerUi from 'swagger-ui-express';
+
+import { OpenApiValidator } from 'express-openapi-validator';
+import { OpenAPIV3 } from 'express-openapi-validator/dist/framework/types'
 import { ForecastController } from './controller/forecast';
 import * as database from '@src/database';
 import { BeachesController } from './controller/beaches';
 import { UsersController } from './controller/users';
 import logger from './logger';
+import { apiErrorValidator } from './middlewares/api-error-validator';
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
@@ -18,8 +24,10 @@ export class SetupServer extends Server {
 
   public async init(): Promise<void> {
     this.setupExpress();
+    await this.docsSetup();
     this.setupControllers();
     await this.databaseSetup();
+    this.setupErrorHandlers();
   }
 
   private setupExpress(): void {
@@ -32,12 +40,27 @@ export class SetupServer extends Server {
     }))
   }
 
+  private setupErrorHandlers(): void {
+    this.app.use(apiErrorValidator);
+  }
+
   private setupControllers(): void {
     const forecastController = new ForecastController();
     const beachesController = new BeachesController();
     const usersController = new UsersController();
 
     this.addControllers([forecastController, beachesController, usersController]);
+  }
+
+  private async docsSetup(): Promise<void> {
+    this.app.use('/docs', swaggerUi.serve, swaggerUi.setup(apiSchema));
+    await new OpenApiValidator(
+      {
+        apiSpec: apiSchema as OpenAPIV3.Document,
+        validateRequests: true,
+        validateResponses: true,
+      }
+    ).install(this.app);
   }
 
   private async databaseSetup() {
